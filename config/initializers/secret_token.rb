56eea93a4b872a6737600c34ee42e6fab4cacefc
Kenny De la cruz <kederu84@gmail.com>
2012-07-17 11:57:05 -0500

    Primer commit en grupo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ponteenonda::Application.config.secret_token = 'b90bda45bdfd54bbe76a140da21712c7124ff71d1809cbd5ed3c1cb7af63e69e18de1848e3b7104d5e40ea89896c3e1f3f736ff5aa35f6f692a060977afe95dd'
